23d8a67e2d9482d2ed374d50525f01a8559843a9
Eunhye Lee <eun020807@naver.com>
2021-12-12 20:16:42 +0900

    211212

---
+++ 16_09/njs2.js
@@ -0,0 +1,4 @@
+document.write("njs2.js");
+document.write("<div style = 'color : blue ; font-size : 24px'>외부 스크립트 파일</div>");
+document.write("<script src = 'njs3.js'> </script>");
+alert ('Nested Script File : 내포된 자바스크립트');
---
+++ 16_09/njs3.js
@@ -0,0 +1,2 @@
+document.write("njs3.js");
+document.write("<div style = 'color : green ; font-size : 24px'>외부 스크립트 파일</div>");
